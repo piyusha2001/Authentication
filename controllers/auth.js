117df2da94375f0c9f2544aab429b799435601b2
piyusha2001 <ppiyusha2001@gmail.com>
2022-04-21 03:23:28 +0530

    sendmail  & verifymailtoken logic added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,70 @@
-const User = require('../models/user');
+const { User } = require('../models/User');
+const MailToken = require('../models/verifyMailToken');
 const ErrorResponse = require('../utils/errorResponse');
+const sendEmail = require('../utils/sendEmail');
+const crypto = require('crypto');
 exports.register = async (req, res, next) => {
+	//get req body
 	const { firstName, lastName, userName, email, password } = req.body;
 	try {
-		const user = await User.create({
+		let user = await User.findOne({ email: email });
+		if (user) {
+			return next(
+				new ErrorResponse('User with this email already exists', 401),
+			);
+		}
+
+		user = await User.create({
 			firstName,
 			lastName,
 			userName,
 			email,
 			password,
 		});
-		res.status(201).json({
-			message: 'User created successfully',
-			userId: user,
+		console.log(user);
+		const mailtoken = await new MailToken({
+			userId: user._id,
+			mailtoken: crypto.randomBytes(32).toString('hex'),
+		}).save();
+
+		console.log(mailtoken);
+
+		const url = `${process.env.BASE_URL}users/${user.id}/verify/${mailtoken.mailtoken}`;
+		await sendEmail(user.email, 'Verify Email', url);
+
+		res.status(201).send({
+			message: 'An Email sent to your account please verify',
 		});
 	} catch (err) {
+		console.log(err);
+	}
+};
+
+exports.verifyMailToken = async (req, res, next) => {
+	const user = await User.findOne({ _id: req.params.id });
+	try {
+		if (!user)
+			return res
+				.status(400)
+				.send({ success: false, message: 'Invalid link' });
+
+		const mailtoken = await MailToken.findOne({
+			userId: user._id,
+			mailtoken: req.params.token,
+		});
+		if (!mailtoken)
+			return res
+				.status(400)
+				.send({ success: false, message: 'Invalid link' });
+
+		await User.findOneAndUpdate({ _id: user._id }, { verified: true });
+		await token.remove();
+
+		res.status(200).send({
+			success: true,
+			message: 'Email verified successfully',
+		});
+	} catch (error) {
 		next(error);
 	}
 };
