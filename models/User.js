f3a8ce6bce61605ad734defce0554889de38f758
piyusha2001 <ppiyusha2001@gmail.com>
2022-04-21 00:05:11 +0530

    hashed password before saving

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema(
 	{
 		firstName: {
@@ -42,5 +42,12 @@ const userSchema = new mongoose.Schema(
 	},
 	{ timestamps: true },
 );
-
+userSchema.pre('save', async function (next) {
+	if (!this.isModified('password')) {
+		return next();
+	}
+	const salt = await bcrypt.genSalt(10);
+	this.password = await bcrypt.hash(this.password, salt);
+	next();
+});
 module.exports = mongoose.model('User', userSchema);
