ee68013d245f8662dcd05205894dceab32ee75a2
piyusha2001 <ppiyusha2001@gmail.com>
2022-04-21 00:05:50 +0530

    user created in register controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,25 @@
-exports.register = (req, res, next) => {
-	res.send('Register route');
+const User = require('../models/user');
+
+exports.register = async (req, res, next) => {
+	const { firstName, lastName, userName, email, password } = req.body;
+	try {
+		const user = await User.create({
+			firstName,
+			lastName,
+			userName,
+			email,
+			password,
+		});
+		res.status(201).json({
+			message: 'User created successfully',
+			userId: user._id,
+		});
+	} catch (err) {
+		res.status(500).json({
+			success: false,
+			error: err.message,
+		});
+	}
 };
 
 exports.login = (req, res, next) => {
