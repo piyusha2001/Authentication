6e331e0e366c73ff4d3f15a09b8e0358a15e9f3a
piyusha2001 <ppiyusha2001@gmail.com>
2022-04-20 22:00:19 +0530

    initial server set up done

---
+++ server.js
@@ -0,0 +1,8 @@
+require('dotenv').config({ path: './' });
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+	console.log(`Server listening on port ${PORT} `);
+});
