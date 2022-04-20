fc357bcd3ba1b1ee9258a1347b53b159ead3823f
piyusha2001 <ppiyusha2001@gmail.com>
2022-04-21 01:56:37 +0530

    method to generate auth token created

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const userSchema = new mongoose.Schema(
 	{
 		firstName: {
@@ -50,4 +51,15 @@ userSchema.pre('save', async function (next) {
 	this.password = await bcrypt.hash(this.password, salt);
 	next();
 });
-module.exports = mongoose.model('User', userSchema);
+
+userSchema.methods.matchPasswords = async function (password) {
+	return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.getSignedToken = function () {
+	return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+		expiresIn: '7d',
+	});
+};
+
+mongoose.model('User', userSchema);
