2cec6e1ac51d0bc12ce667fb363a00535e248344
piyusha2001 <ppiyusha2001@gmail.com>
2022-04-20 22:37:19 +0530

    imported auth routes

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 require('dotenv').config({ path: './' });
 const express = require('express');
 const app = express();
+const authRoutes = require('./routes/auth');
+
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
