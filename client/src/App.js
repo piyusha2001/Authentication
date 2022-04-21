432f7d3598f2dcaea4954c45e1989a4a161c351c
piyusha2001 <ppiyusha2001@gmail.com>
2022-04-21 21:09:08 +0530

    profile route added

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import PrivateRoute from './components/routing/PrivateRoute';
 import EmailVerify from './components/Screens/EmailVerify';
 import LoginScreen from './components/Screens/LoginScreen';
 import PrivateScreen from './components/Screens/PrivateScreen';
+import Profile from './components/Screens/Profile.jsx';
 import RegisterScreen from './components/Screens/RegisterScreen';
 
 function App() {
@@ -21,6 +24,7 @@ function App() {
 			<Route exact path='/login' element={<LoginScreen />} />
 			<Route exact path='/register' element={<RegisterScreen />} />
 			<Route path='users/:id/verify/:token' element={<EmailVerify />} />
+			<Route exact path='/profile' element={<Profile />} />
 		</Routes>
 	);
 }
