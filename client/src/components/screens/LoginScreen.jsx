9ea900b429433930d84ae0413ba1951e6cae1cbd
piyusha2001 <ppiyusha2001@gmail.com>
2022-04-21 21:08:37 +0530

    logout functionality added and myprofile linked

--- client/src/components/screens/LoginScreen.jsx
+++ client/src/components/screens/LoginScreen.jsx
@@ -1,12 +1,47 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Login.module.css';
 
 const LoginScreen = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [error, setError] = useState('');
+
+	useEffect(() => {
+		if (localStorage.getItem('authToken')) {
+			window.location.href = '/';
+		}
+	}, []);
+
+	const loginHandler = async (e) => {
+		e.preventDefault();
+		try {
+			const { data } = await axios.post(
+				'http://localhost:8080/api/auth/login',
+
+				{
+					email,
+					password,
+				},
+			);
+			console.log(data);
+			localStorage.setItem('authToken', data.token);
+			localStorage.setItem('userData', JSON.stringify(data.user));
+			window.location.href = '/';
+		} catch (err) {
+			setError(err);
+		}
+	};
+
 	return (
 		<div className={styles.login_container}>
 			<div className={styles.login_form_container}>
 				<div className={styles.left}>
-					<form className={styles.form_container}>
+					<form
+						onSubmit={loginHandler}
+						className={styles.form_container}
+					>
 						<h1>Login to Your Account</h1>
 						<input
 							type='email'
@@ -14,6 +49,8 @@ const LoginScreen = () => {
 							name='email'
 							required
 							className={styles.input}
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
 						/>
 						<input
 							type='password'
@@ -21,8 +58,12 @@ const LoginScreen = () => {
 							name='password'
 							required
 							className={styles.input}
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
 						/>
-
+						{error && (
+							<div className={styles.error_msg}>{error}</div>
+						)}
 						<button type='submit' className={styles.red_btn}>
 							Login
 						</button>
