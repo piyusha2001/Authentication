b085fa9adac6d9995f3dcc91b42f65bd55c90d50
piyusha2001 <ppiyusha2001@gmail.com>
2022-04-21 12:38:05 +0530

    created routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+import PrivateRoute from './components/routing/PrivateRoute';
+import PrivateScreen from './components/screens/PrivateScreen';
+import LoginScreen from './components/screens/LoginScreen';
+import RegisterScreen from './components/screens/RegisterScreen';
 function App() {
-	return <div className='App'>React App</div>;
+	return (
+		<Routes>
+			<div className='App'>
+				<PrivateRoute exact path='/' component={PrivateScreen} />
+				<Route exact path='/login' component={LoginScreen} />
+				<Route exact path='/register' component={RegisterScreen} />
+			</div>
+		</Routes>
+	);
 }
 
 export default App;
